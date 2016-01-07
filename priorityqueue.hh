e1ebd8e70cf415bab457847fc82db2363e4634f4
Piotr Wisniewski <pw361286@students.mimuw.edu.pl>
2016-01-07 18:35:33 +0100

    Cos jest nie tak z operatorem =, korzystam z faktu ze K i V maja zdefiniowane == trzeba sie tego pozbyc

--- priorityqueue.hh
+++ priorityqueue.hh
@@ -3,7 +3,15 @@
 #include <memory>
 #include <utility>
 
-class PriorityQueueEmptyException {
+class PriorityQueueEmptyException : public std::exception {
+    public:
+    virtual const char* what() const throw()
+    {
+        return "My exception happened";
+    }
+};
+
+class PriorityQueueNotFoundException : public std::exception {
     public:
     virtual const char* what() const throw()
     {
@@ -31,7 +39,7 @@ class PriorityQueue {
 
         struct value_first_cmp {
             bool operator() (const key_value_ptr& a, const key_value_ptr& b) const {
-                return a->second == b->second ? a->first < b->second : a->second < b->second;
+                return a->second == b->second ? a->first < b->first : a->second < b->second;
             }
         };
 
@@ -60,6 +68,7 @@ class PriorityQueue {
         return values.empty();
     }
 
+    //byc moze kazdy wyjatek trzeba lapac a potem rzucac ponownie - mozliwe ze jak nie zlapie to nie wywoluja sie destruktory
     void insert(const K& key, const V& value) {
         key_value_pair to_add(key, value);
         key_value_ptr to_add_ptr = std::make_shared<key_value_pair>(to_add);
@@ -88,25 +97,25 @@ class PriorityQueue {
 
     const V& minValue() const {
         if (empty())
-            throw new PriorityQueueEmptyException();
+            throw PriorityQueueEmptyException();
         return (*(values.begin()))->second;
     }
 
     const V& maxValue() const {
         if (empty())
-            throw new PriorityQueueEmptyException();
+            throw PriorityQueueEmptyException();
         return (*(values.rbegin()))->second;
     }
 
     const K& minKey() const {
         if (empty())
-            throw new PriorityQueueEmptyException();
+            throw PriorityQueueEmptyException();
         return (*(values.begin()))->first;
     }
 
     const K& maxKey() const {
         if (empty())
-            throw new PriorityQueueEmptyException();
+            throw PriorityQueueEmptyException();
         return (*(values.rbegin()))->first;
     }
 
@@ -126,20 +135,133 @@ class PriorityQueue {
         typename value_set::iterator to_delete_it_v = values.end();
         to_delete_it_v--;
         typename key_set::iterator to_delete_it_k = keys.lower_bound(*to_delete_it_v);
-        to_delete_it_k--;
+        //UWAGA to nizej chyba nie potrzebne
+        //to_delete_it_k--;
         //erase jest no_throw jezeli dajemy mu iterator
         values.erase(to_delete_it_v);
         keys.erase(to_delete_it_k);
     }
 
     void changeValue(const K& key, const V& value) {
+        if (empty())
+            throw PriorityQueueNotFoundException();
+        key_value_pair to_add(key, value);
+        key_value_ptr to_add_ptr = std::make_shared<key_value_pair>(to_add);
+        typename key_set::iterator to_remove_it_k = keys.lower_bound(to_add_ptr);
 
+        if (to_remove_it_k == keys.end() || !((*to_remove_it_k)->first == key))
+            to_remove_it_k--;
+        if (**to_remove_it_k == to_add)
+            return;
+        if (!((*to_remove_it_k)->first == key))
+            throw PriorityQueueNotFoundException();
+
+        typename value_set::iterator to_remove_it_v = values.find(*to_remove_it_k);
+
+        //dodajemy ptr
+
+        typename value_set::iterator added_v = values.insert(to_add_ptr);
+        try {
+            keys.insert(to_add_ptr);
+        }
+        catch (...) {
+            values.erase(added_v);
+            throw;
+        }
+        keys.erase(to_remove_it_k);
+        values.erase(to_remove_it_v);
     }
 
     void swap(PriorityQueue<K, V>& queue) {
         values.swap(queue.values);
         keys.swap(queue.keys);
     }
+
+    void merge(PriorityQueue<K, V>& queue) {
+        //dodajemy do nowych setow - jak sie wysypie to sie nic nie stanie bo nie dotykamy naszych wenwetrznych
+        if (this == &queue)
+            return;
+        value_set new_values(this->values);
+        key_set new_keys(this->keys);
+        for (auto to_add : queue.values) {
+            auto it = this->values.find(to_add);
+            if (it != this->values.end()) {
+                new_values.insert(*it);
+                new_keys.insert(*it);
+            }
+            else {
+                new_values.insert(to_add);
+                new_keys.insert(to_add);
+            }
+        }
+        //dodawanie sie udalo wiec mozemy umiescic te sety w naszej kolejsce
+        this->values.swap(new_values);
+        this->keys.swap(new_keys);
+        if (this != &queue)
+            queue.clear();
+    }
+
+    void clear() {
+        values.clear();
+        keys.clear();
+    }
+
+    PriorityQueue<K, V>& operator=(PriorityQueue<K, V> queue) {
+        this->swap(queue);
+        return *this;
+    }
+
+    bool operator==(const PriorityQueue<K, V>& queue) const {
+        if (this->size() != queue.size())
+            return false;
+        typename value_set::iterator this_it = this->values.begin();
+        typename value_set::iterator queue_it = queue.values.begin();
+        while (this_it != this->values.end()) {
+            if (**this_it != **queue_it)
+                return false;
+            this_it++;
+            queue_it++;
+        }
+        return true;
+    }
+
+    bool operator!=(const PriorityQueue<K, V>& queue) const {
+        return !(*this == queue);
+    }
+
+    bool operator<(const PriorityQueue<K, V>& queue) const {
+        typename value_set::iterator this_it = this->values.begin();
+        typename value_set::iterator queue_it = queue.values.begin();
+        while (this_it != this->values.end() && queue_it != queue.values.end()) {
+            if (**this_it != **queue_it) {
+                if (**this_it < **queue_it)
+                    return true;
+                else
+                    return false;
+            }
+            this_it++;
+            queue_it++;
+        }
+        if (this_it == this->values.end() && queue_it == queue.values.end())
+            return false;
+        else if (this_it == this->values.end())
+            return true;
+        else
+            return false;
+    }
+
+    bool operator<=(const PriorityQueue<K, V>& queue) const {
+        return *this == queue || *this < queue;
+    }
+
+    bool operator>(const PriorityQueue<K, V>& queue) const {
+        return !(*this <= queue);
+    }
+
+    bool operator>=(const PriorityQueue<K, V>& queue) const {
+        return !(*this < queue);
+    }
+
 };
 
 template <typename K, typename V>
@@ -147,62 +269,6 @@ void swap(PriorityQueue<K, V>& a, PriorityQueue<K, V>& b) {
     a.swap(b);
 }
 
-template <typename K, typename V>
-bool operator==(const PriorityQueue<K, V>& a, const PriorityQueue<K, V>& b) {
-    if (a.size() != b.size())
-        return false;
-    value_set::iterator a_it = a.values.begin();
-    value_set::iterator b_it = b.values.begin();
-    while (a_it != a.end()) {
-        if (**a_it != **b_it)
-            return false;
-        a_it++;
-        b_it++;
-    }
-    return true;
-}
-
-template <typename K, typename V>
-bool operator!=(const PriorityQueue<K, V>& a, const PriorityQueue<K, V>& b) {
-    return !(a == b);
-}
-
-template <typename K, typename V>
-bool operator<(const PriorityQueue<K, V>& a, const PriorityQueue<K, V>& b) {
-    value_set::iterator a_it = a.values.begin();
-    value_set::iterator b_it = b.values.begin();
-    while (a_it != a.values.end() && b_it != b.values.end()) {
-        if (**a_it != **b_it) {
-            if (**a_it < **b_it)
-                return true;
-            else
-                return false;
-        }
-        a_it++;
-        b_it++;
-    }
-    if (a_it == a.values.end() && b_it == b.values.end())
-        return false;
-    else if (a_it == a.values.end())
-        return true;
-    else
-        return false;
-}
-
-template <typename K, typename V>
-bool operator<=(const PriorityQueue<K, V>& a, const PriorityQueue<K, V>& b) {
-    return a == b || a < b;
-}
-
-template <typename K, typename V>
-bool operator>(const PriorityQueue<K, V>& a, const PriorityQueue<K, V>& b) {
-    return !(a <= b);
-}
-
-template <typename K, typename V>
-bool operator>=(const PriorityQueue<K, V>& a, const PriorityQueue<K, V>& b) {
-    return !(a < b);
-}
 
 
 
